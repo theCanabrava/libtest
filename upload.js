ebb8c5027b73df0d798ce57d2714dd4540e40c6d
thecanabrava <vcanabravatr@gmail.com>
2021-03-10 20:17:49 -0300

    Credenciais aws como variaveis

--- upload.js
+++ upload.js
@@ -1,18 +1,19 @@
 const s3FolderUpload = require('s3-folder-upload')
-// or the ES6 way
-// import s3FolderUpload from 's3-folder-upload'
+
+const accessKeyId = process.argv[process.argv.findIndex(a => a === '--id') + 1];
+const accessKeySecret = process.argv[process.argv.findIndex(a => a === '--secret') + 1];
 
 const directoryName = 'dist'
 // I strongly recommend to save your credentials on a JSON or ENV variables, or command line args
 const credentials = {
-  "accessKeyId": "AKIA3AQGWMKLAVLVVBM7",
-  "secretAccessKey": "J2u51qVNPs+5NAZkfSRuvLx/SL9CzLk0sV3711kC",
-  "region": "us-west-2",
-  "bucket": "thecanabrava-libtest"
+  accessKeyId: accessKeyId,
+  secretAccessKey: accessKeySecret,
+  region: "us-west-2",
+  bucket: "thecanabrava-libtest"
 }
 const options = {
     useFoldersForFileTypes: false,
 }
 
-
+console.log(credentials);
 s3FolderUpload(directoryName, credentials, options)
