2a41c29dd821cb00fa582f5ae17e701d5f81c6ce
thecanabrava <vcanabravatr@gmail.com>
2021-03-12 12:05:28 -0300

    Troca de strings

--- src/screens/strings/index.js
+++ src/screens/strings/index.js
@@ -2,7 +2,7 @@ export default
 {
     USER: 'Usuário:',
     PASS: 'Senha:',
-    LOGIN: 'LOGAR',
+    LOGIN: 'ACESSAR CONTA',
 
     EXCLUSIVE_A: 'TELA EXCLUSIVA A',
     EXCLUSIVE_B: 'TELA EXCLUSIVA B',
